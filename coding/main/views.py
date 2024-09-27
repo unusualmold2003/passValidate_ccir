8edafe43737e171ac14e9b784958de5668973c85
bharatanaik <bharat.anaik2003@gmail.com>
2024-09-27 14:53:54 +0530

    add urls and views.py

---
+++ coding/main/views.py
@@ -0,0 +1,12 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+
+
+# password strength checker LOgic over here
+
+# ENDPOINT :  POST /password-strength 
+
+def password_strength(request):
+    
+    return JsonResponse({})
