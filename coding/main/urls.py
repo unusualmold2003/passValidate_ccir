fab9aee6537bccdf63f8e1796d699b5bbb30497a
bharatanaik <bharat.anaik2003@gmail.com>
2024-09-30 14:07:25 +0530

    final

--- coding/main/urls.py
+++ coding/main/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-
-
-
+from main import views
 
 urlpatterns = [
-
     # Add password validator endpoint here
 ]
--- coding/main/views.py
+++ coding/main/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest, JsonResponse
+from django.contrib import messages
 
-def password_validator(request):
-    if request.method == 'POST':
-        password = request.POST.get('password')
+
+
+def index(request:HttpRequest):
+    if request.method == "POST":
+        password = request.POST.get("password")
         if validate_password(password):
-            return HttpResponse("Password is valid")
+            messages.success(request, "Your password strength is GOOD")
         else:
-            return HttpResponse("Password is invalid")
-    return render(request, 'password_validator.html')
+            messages.success(request, "Ah! Please try a new password as it's not secure")
+    return render(request, "main/index.html")
 
+    
 def validate_password(password):
-    password = request.GET.get('password', '')
-    result = zxcvbn(password)
-    return JsonResponse(result)
     if len(password) < 8:
         return False
     if not any(char.isdigit() for char in password):
@@ -21,3 +22,7 @@ def validate_password(password):
     if not any(char.isalpha() for char in password):
         return False
     return True
+
+
+
+    
